33688657a16a6440299635a73bb21d9179c46a30
CloudySkyLoftyMoon <jonariluec@stu.ncu.edu.jm>
2025-04-08 00:07:31 +0000

    Initial commit.

---
+++ PizzaClient-Original/PizzaClient/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Pizza from './Pizza';
+
+ReactDOM.createRoot(document.getElementbyId('root')).render(
+  <React.StrictMode>
+    <Pizza />
+  </React.StrictMode>,
+)
